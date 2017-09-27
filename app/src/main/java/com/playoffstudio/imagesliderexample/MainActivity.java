7ca74c3d95efd028acc51c8bbe343910728a6f68
pavelcep.sust11@gmail.com <pavelandroid11>
2017-09-27 23:32:35 +0600

    Initial commit

---
+++ app/src/main/java/com/playoffstudio/imagesliderexample/MainActivity.java
@@ -0,0 +1,94 @@
+package com.playoffstudio.imagesliderexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    String[] titles = {
+            "Android",
+            "Beta",
+            "Cupcake",
+            "Donut"
+
+    };
+
+
+    String[] imagess = {
+            "https://www.w3schools.com/w3images/fjords.jpg",
+            "https://www.w3schools.com/w3images/fjords.jpg",
+            "https://www.w3schools.com/w3images/fjords.jpg",
+            "https://www.w3schools.com/w3images/fjords.jpg"
+    };
+
+
+    private SliderLayout mDemoSlider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
+
+
+        for (int i=0 ; i<=3 ; i++){
+            TextSliderView textSliderView = new TextSliderView(getApplicationContext());
+            textSliderView.description(titles[i])
+                    .image(imagess[i])
+                    .setScaleType(BaseSliderView.ScaleType.Fit);
+
+            //add your extra information
+            mDemoSlider.addSlider(textSliderView);
+
+        }
+
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+
+        
+    }
+
+    @Override
+    protected void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
